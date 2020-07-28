ad062bd6803250ca894cb88fbc7c457829ad5fd2
harrypotterrrr <jiahaolin19971119@126.com>
2020-07-28 14:42:31 +0800

    update main.py with fresh params

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ['CUDA_VISIBLE_DEVICES'] = '2, 3'
+
+from data_loader.data_loader import DatasetGenerator
+from models.EDVR import EDVR
+from trainers.train import Trainer
+from utils.config import process_config
+
+# config_path = "configs/config.json"
+
+def main():
+    config = process_config()
+    train_data = DatasetGenerator(config)()
+    model = EDVR(config)
+
+    trainer = Trainer(model, train_data, config)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
