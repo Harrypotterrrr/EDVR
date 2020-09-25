66912ae541c0a97de9acd9c085ebad48040d9cab
harrypotterrrr <jiahaolin19971119@126.com>
2020-09-25 11:14:39 +0800

    add TFRecord to process image dataset to accelerate

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -7,22 +7,29 @@ import numpy as np
 import tensorflow as tf
 
 
-class DatasetGenerator:
-    def __init__(self, config):
-        # data, info = tfds.load(name="mnist", download=True, with_info=True)
-        # self.train_data = data['train']
-        # self.test_data = data['test']
+class REDSDataLoader:
 
-        # self.config = config
-        # assert isinstance(self.train_data, tf.data.Dataset)
+    def __init__(self, config):
 
         self.nframes = config["nframes"]
         self.center = self.nframes // 2
         self.batch_size = config["batch_size"]
 
-        root_path = pathlib.Path(config["root_path"])
-        train_path = root_path.joinpath('train')
+        self.root_path = config["root_path"]
+        self.train_path = os.path.join(self.root_path, 'train')
+        self.val_path = os.path.join(self.root_path, 'val')
+        self.test_path = os.path.join(self.root_path, 'test')
 
+        self.tfrecord_path = config["tfrecord_path"]
+
+        if not os.path.exists(self.tfrecord_path):
+            x_paths, y_paths = self.build_image_paths()
+            self.build_tfrecord(x_paths, y_paths)
+
+    def build_image_paths(self): # train_blur_bicubic robustness coding TODO
+
+        train_path = pathlib.Path(self.train_path)
+        # self.train_path = root_path.joinpath('train')
 
         train_blur_bicubic_paths = []
         train_blur_bicubic_subpath = list(sorted(train_path.glob(f'{config["train_blur_bicubic"]}*')))
@@ -30,12 +37,9 @@ class DatasetGenerator:
             label = pathlib.Path(subpath).name
             train_blur_bicubic_subpath = [str(path) for path in train_path.glob(f'{config["train_blur_bicubic"]}{label}/*')]
             train_blur_bicubic_subpath = self.build_clips(sorted(train_blur_bicubic_subpath))
-            # print(train_blur_bicubic_subpath)
-            train_blur_bicubic_paths.append(train_blur_bicubic_subpath)
+            train_blur_bicubic_paths.extend(train_blur_bicubic_subpath)
 
-        train_blur_bicubic_paths = np.asarray(train_blur_bicubic_paths).reshape(-1, self.nframes)
-
-        train_blur_paths = list(sorted(train_path.glob(f'{config["train_blur_bicubic"]}*/*')))
+        train_blur_paths = list(sorted(train_path.glob(f'{config["train_blur"]}*/*')))
         train_blur_paths = [str(path) for path in train_blur_paths]
 
         train_blur_bicubic_ctr = len(train_blur_bicubic_paths)
@@ -43,11 +47,10 @@ class DatasetGenerator:
 
         # print(train_blur_ctr, train_blur_bicubic_ctr)
         assert train_blur_ctr == train_blur_bicubic_ctr
-        train_blur_bicubic_paths = train_blur_bicubic_paths[:10] # TODO
-        train_blur_paths = train_blur_paths[:10]
+        return train_blur_bicubic_paths, train_blur_paths
 
-        # print(len(train_blur_bicubic_paths), len(train_blur_paths))
-        ds = tf.data.Dataset.from_tensor_slices((train_blur_bicubic_paths, train_blur_paths))
+    def build_dataloader(self, x_paths, y_paths): # deprecated
+        ds = tf.data.Dataset.from_tensor_slices((x_paths, y_paths))
 
         def convert_types(image):
             image = tf.cast(image, tf.float32)
@@ -55,33 +58,37 @@ class DatasetGenerator:
             return image
 
         def load_image(x_path, y_path):
-            # assert len(x_path) == self.nframes
-
-            # x_image = []
-            # for i in x_path:
-            #     x = tf.io.read_file(i)
-            #     x_image.append(tf.image.decode_image(x))
-            # x_image = tf.convert_to_tensor(x_image, dtype=tf.float32)
-
-            x_image1 = convert_types(tf.image.decode_image(tf.io.read_file(x_path[0]), channels=3))
-            x_image2 = convert_types(tf.image.decode_image(tf.io.read_file(x_path[1]), channels=3))
-            x_image3 = convert_types(tf.image.decode_image(tf.io.read_file(x_path[2]), channels=3))
-            x_image4 = convert_types(tf.image.decode_image(tf.io.read_file(x_path[3]), channels=3))
-            x_image5 = convert_types(tf.image.decode_image(tf.io.read_file(x_path[4]), channels=3)) # disgusting decorator @tf.funciton TODO
-            x_image = tf.stack([x_image1, x_image2, x_image3, x_image4, x_image5])
+            x_image = []
+            for i in x_path:
+                x = tf.io.read_file(i)
+                x_image.append(tf.image.decode_image(x, channels=3))
+            x_image = tf.stack(x_image, dtype=tf.float32)
 
             y = tf.io.read_file(y_path)
             y_image = tf.image.decode_image(y, channels=3)
 
-            # image = tf.image.resize(image, [192, 192])
-            # image /= 255.0
             return x_image, y_image
 
-        # for step, (inputs, targets) in enumerate(ds):
-        #     load_and_preprocess_from_path_label(inputs, targets)
+        self.dataset = ds.map(load_image)
+        self.dataset = self.ds.shuffle(len(ds)).batch(self.batch_size)  # .repeat(10)
 
-        ds = ds.map(load_image)
-        self.ds = ds.shuffle(len(ds)).batch(self.batch_size) #.repeat(10)
+    def build_tfrecord(self, x_paths, y_paths):
+
+        with tf.io.TFRecordWriter(self.tfrecord_path) as writer:
+            for x_path, y_path in zip(x_paths, y_paths):
+                y_path = open(y_path, 'rb').read()
+                feature = {
+                    'nframes': tf.train.Feature(int64_list=tf.train.Int64List(value=[self.nframes])),
+                    'target': tf.train.Feature(bytes_list=tf.train.BytesList(value=[y_path]))
+                }
+
+                for i, _x in enumerate(x_path):
+                    frame = open(_x, 'rb').read()
+                    feature[f'frame{i}'] = tf.train.Feature(bytes_list=tf.train.BytesList(value=[frame]))
+
+                example = tf.train.Example(features=tf.train.Features(feature=feature))
+                writer.write(example.SerializeToString())
+            writer.close()
 
     def build_clips(self, image_list: list):
         image_list = np.asarray(image_list)
@@ -144,14 +151,40 @@ class DatasetGenerator:
 
     def __call__(self):
         # self.preprocess()
-        return self.ds
+        self.decode_tfrecord()
+        return self.dataset
+
+    def decode_tfrecord(self):
+        self.dataset = tf.data.TFRecordDataset(self.tfrecord_path)
+        feature_description = {
+            'nframes': tf.io.FixedLenFeature([], tf.int64),
+            'target': tf.io.FixedLenFeature([], tf.string)
+        }
+        for i in range(self.nframes):
+            feature_description[f'frame{i}'] = tf.io.FixedLenFeature([], tf.string)
+
+        def _load_image(example_string):
+            feature_dict = tf.io.parse_single_example(example_string, feature_description)
+            images = []
+            for i in range(self.nframes):
+                image = tf.io.decode_png(feature_dict[f'frame{i}'])
+                image = tf.cast(image, dtype='float32') / 255.
+                images.append(image)
+            images = tf.stack(images)
+            target = tf.io.decode_png(feature_dict['target'])
+            target = tf.cast(target, dtype='float32') / 255.
+            return images, target
+
+        self.dataset = self.dataset.map(_load_image)  # 解析数据
+        self.dataset = self.dataset.shuffle(buffer_size=100)  # 在缓冲区中随机打乱数据 TODO
+        self.dataset = self.dataset.batch(batch_size=self.batch_size)
 
     def preprocess(self):
         self.train_data = self.train_data.map(
-            DatasetGenerator.convert_types
+            REDSDataLoader.convert_types
         ).batch(self.config.batch_size)
         self.test_data = self.test_data.map(
-            DatasetGenerator.convert_types
+            REDSDataLoader.convert_types
         ).batch(self.config.batch_size)
 
     @staticmethod
@@ -162,12 +195,15 @@ class DatasetGenerator:
         return image, label
 
 if __name__ == "__main__":
-    dataset = DatasetGenerator(None)()
+    from utils.config import process_config
+    config = process_config()
 
+    dataloader = REDSDataLoader(config)
+    for step, (inputs, targets) in enumerate(dataloader()):
+        print(inputs.shape)
+        print(targets.shape)
 
-    from models.EDVR import EDVR
-    model = EDVR()
-    for step, (inputs, targets) in enumerate(dataset):
-        y = model(inputs)
-
-
+    # from models.EDVR import EDVR
+    # model = EDVR()
+    # for step, (inputs, targets) in enumerate(dataloader()):
+    #     y = model(inputs)
