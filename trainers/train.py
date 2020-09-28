ba902bacb3dfb4d736f2d442841a15fab5cdc3b2
harrypotterrrr <jiahaolin19971119@126.com>
2020-09-29 01:47:03 +0800

    update checkpoint & ckpManager

--- trainers/train.py
+++ trainers/train.py
@@ -9,6 +9,8 @@ class Trainer:
     def __init__(self, config, model, dataloader):
         self.config = config
 
+
+
         self.pretrained_model = self.config["pretrained_model"]
 
         self.batch_size = self.config["batch_size"]
@@ -30,6 +32,15 @@ class Trainer:
         self.model = model(config)
         self.optimizer = tf.optimizers.Adam(learning_rate=self.config["lr"])
         self.dataset = dataloader()
+
+        self.checkpoint = tf.train.Checkpoint(step=tf.Variable(0), optimizer=self.optimizer, model=self.model)
+        self.manager = tf.train.CheckpointManager(self.checkpoint, self.model_save_path, max_to_keep=3)
+
+        if self.pretrained_model is True:
+            self.manager.restore_or_initialize()
+            # print(self.manager.checkpoints) # checkpoints list
+            # self.checkpoint.restore(tf.train.latest_checkpoint(self.model_save_path))
+            print(f"Restored from {self.manager.latest_checkpoint}")
 
     def calc_psnr(self, pred, target, max_val=1.0): # TODO max_val
         psnr = tf.image.psnr(pred, target, max_val=max_val)
@@ -60,6 +71,7 @@ class Trainer:
         psnr_list = []
         start_time = time.time()
         for step, (batch_x, batch_y) in enumerate(self.dataset):
+            self.checkpoint.step.assign_add(1)
             loss, psnr = self.train_step(batch_x, batch_y)
 
             elapsed, start_time = self.calc_time(start_time)
@@ -71,6 +83,9 @@ class Trainer:
 
             loss_list.append(loss)
             psnr_list.append(psnr)
+
+            if step % self.model_save_epoch == 0:
+                self.manager.save()  # save checkpoint
 
         loss = np.mean(loss_list)
         psnr = np.mean(psnr_list)
