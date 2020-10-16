73a2b4eeb9efa54be18364e841e8ddf61e98b366
harrypotterrrr <jiahaolin19971119@126.com>
2020-10-16 21:18:44 +0800

    fix top emergency & long consistency bug: TensorArray size type wrong

--- models/TSA.py
+++ models/TSA.py
@@ -10,6 +10,7 @@ class TSA_Fusion(tf.keras.Model):
     def __init__(self, nf=64, nframes=5, center=2):
         super(TSA_Fusion, self).__init__()
 
+        self.nframes = nframes
         self.center = center
         # temporal attention (before fusion conv)
         self.tAtt_1 = keras.layers.Conv2D(nf, (3, 3), (1, 1), "same")
@@ -33,15 +34,19 @@ class TSA_Fusion(tf.keras.Model):
 
     def __call__(self, aligned_fea):
 
-        aligned_fea_shape = tf.shape(aligned_fea)
+        aligned_fea_shape = tf.shape(aligned_fea) # B, N, H, W, C
         B = aligned_fea_shape[0]
         N = aligned_fea_shape[1]
         H = aligned_fea_shape[2]
         W = aligned_fea_shape[3]
         C = aligned_fea_shape[4]
-        emb_ref = self.tAtt_2(aligned_fea[:, self.center, :, :, :])
-        emb = tf.reshape(self.tAtt_1(tf.reshape(aligned_fea, [-1, H, W, C])), [B, N, H, W, -1])
-        cor_l = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+
+        emb_ref = self.tAtt_1(aligned_fea[:, self.center, :, :, :]) # B, H, W, C
+        emb = tf.reshape(aligned_fea, [-1, H, W, C]) # BN, H, W, C
+        emb = self.tAtt_2(emb)
+        emb = tf.reshape(emb, [B, N, H, W, -1])
+
+        cor_l = tf.TensorArray(dtype=tf.float32, size=self.nframes)
         def cond(i, N, input, arr):
             return tf.less(i, N)
         def body(i, N, input, arr):
@@ -50,14 +55,19 @@ class TSA_Fusion(tf.keras.Model):
             arr = arr.write(i, cor_tmp)
             i = tf.add(i, 1)
             return i, N, input, arr
-        _, _, _, cor_l = tf.while_loop(cond, body, [0, N, emb, cor_l])
-        cor_l = cor_l.stack()
-        cor_l = tf.transpose(cor_l, [1, 0, 2, 3])
+        _, _, _, cor_l = tf.while_loop(cond, body, [0, N, emb, cor_l]) # N * (B, H, W)
+        cor_l = cor_l.stack() # N, B, H, W
+        cor_l = tf.transpose(cor_l, [1, 0, 2, 3]) # B, N, H, W
+
         cor_prob = tf.sigmoid(cor_l)  # B, N, H, W
-        cor_prob = tf.expand_dims(cor_prob, axis=4)
-        cor_prob = tf.tile(cor_prob, [1, 1, 1, 1, C])
-        cor_prob = tf.reshape(cor_prob, [B, H, W, -1])
+        cor_prob = tf.expand_dims(cor_prob, axis=4) # B, N, H, W, 1
+        cor_prob = tf.tile(cor_prob, [1, 1, 1, 1, C]) # B, N, H, W, C
+        cor_prob = tf.transpose(cor_prob, [0, 2, 3, 1, 4]) # B, H, W, N, C
+        cor_prob = tf.reshape(cor_prob, [B, H, W, -1]) # B, H, W, NC
+
+        aligned_fea = tf.transpose(aligned_fea, [0, 2, 3, 1, 4])  # [B, H, W, N, C]
         aligned_fea = tf.reshape(aligned_fea, [B, H, W, -1]) * cor_prob
+
         ### fusion
         fea = self.lrelu(self.fea_fusion(aligned_fea))
         ### spatial attention
@@ -73,6 +83,7 @@ class TSA_Fusion(tf.keras.Model):
         att_L = self.lrelu(self.sAtt_L3(att_L))
         att_L_shape = tf.shape(att_L)
         att_L = tf.image.resize(att_L, [2 * att_L_shape[1], 2 * att_L_shape[2]])
+
         att = self.lrelu(self.sAtt_3(att))
         att = att + att_L
         att = self.lrelu(self.sAtt_4(att))
